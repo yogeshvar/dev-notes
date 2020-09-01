c39bd7ad2f75a84bb875cfa8dfc8ef2709800281
Yogeshvar <yogeshvar@icloud.com>
2020-09-01 23:51:21 +0530

    :art: improve written algos

--- Algorithms/LinearSearch.py
+++ Algorithms/LinearSearch.py
@@ -1,10 +1,8 @@
 arr = [1, 2, 3, 4, 1]
 find_element = 5
 
-for i in range(len(arr)):
-    if arr[i] is find_element:
-        print i
+for i in range(0, len(arr)):
+    if arr[i] == find_element:
+        print(i)
         break
-    else:
-        print -1
-        break
+    
