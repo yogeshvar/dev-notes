b5c0c03b8405b30275a343a7b6624dd2b2410a73
Yogeshvar <yogeshvar@icloud.com>
2021-01-17 20:10:09 +0530

    :sparkles: add triangle pattern & linkedlist cpp

--- DS/Cpp/LinkedList.cpp
+++ DS/Cpp/LinkedList.cpp
@@ -19,15 +19,50 @@ public:
         head = NULL;
     }
     void prepend(int data);
+    void append(int data);
+    void printHead();
     void print();
 };
+
+void LinkedList::printHead()
+{
+    cout << "current head:" << head->data;
+}
 
 void LinkedList::prepend(int data)
 {
     Node<int> *newHead = new Node<int>;
     newHead->data = data;
-    newHead->next = head;
+    if (head)
+        newHead->next = head;
+    else
+        newHead->next = NULL;
     head = newHead;
+}
+
+void LinkedList::append(int data)
+{
+    Node<int> *newTail = new Node<int>;
+    newTail->data = data;
+    newTail->next = NULL;
+
+    if (head->next == NULL)
+    {
+        head = newTail;
+    }
+    else
+    {
+
+        while (head)
+        {
+            if (head->next == NULL)
+            {
+                head->next = newTail;
+                break;
+            }
+            head = head->next;
+        }
+    }
 }
 
 void LinkedList::print()
@@ -42,8 +77,9 @@ void LinkedList::print()
 int main()
 {
     LinkedList list;
-
+    // list.prepend(9);
     list.prepend(10);
-    list.prepend(9);
+    list.append(11);
+    list.append(12);
     list.print();
 }
