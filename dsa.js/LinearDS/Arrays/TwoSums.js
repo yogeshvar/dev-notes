64896e143830e1514688f368db7f7f2b1a3426a9
Maggie Yogeshvar <yogeshvar@icloud.com>
2021-06-30 17:11:41 +0000

    :sparkles: add dsa repo problems

---
+++ dsa.js/LinearDS/Arrays/TwoSums.js
@@ -0,0 +1,14 @@
+arr = [-3, -2, -1, 1, 1, 3, 4];
+target = -4;
+
+console.log(arr, target);
+
+for (var i = 0; i < arr.length; i++) {
+  for (var j = i + 1; j < arr.length; j++) {
+    var sum = arr[i] + arr[j];
+    if (sum == target) {
+      console.log(arr[i], arr[j]);
+      break;
+    }
+  }
+}
