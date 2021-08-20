171b10d9e42e8b793c92fd4ea57a4b9bbbda9611
Yogeshvar <yogeshvar@icloud.com>
2021-08-20 22:53:44 +0530

    :sparkles: add topcoder problems

---
+++ TopCoder/MNS.java
@@ -0,0 +1,19 @@
+public class MNS {
+
+    public static int combos(int[] numbers) {
+
+        return -1;
+    }
+
+    /**
+     * 1 2 3 3 2 1 2 2 2
+     * 1 1 2 2 2 2 2 3 3 
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3, 3, 2, 1, 2, 2, 2 };
+        System.out.println(combos(numbers));
+    }
+
+}
---
+++ TopCoder/SortingSubset.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * Method 1: Brute Force Method 2: Recursion.
+ */
+public class SortingSubset {
+    public static int getMinimalSize(int[] a) {
+        int[] b = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+        Arrays.sort(a);
+        int count = 0;
+        for (int i = 0; i < b.length; i++) {
+            if (a[i] != b[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 3, 2, 1 };
+        System.out.println(getMinimalSize(a));
+    }
+}
