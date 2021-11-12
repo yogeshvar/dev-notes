4af3fde2e0df107c75c886f2ece6c1556eec831b
Yogeshvar <yogeshvar@icloud.com>
2021-11-12 12:59:18 +0530

    isUnique prob modified

--- CrackingCodingInterview/IsUnique.java
+++ CrackingCodingInterview/IsUnique.java
@@ -1,12 +1,16 @@
-package InterviewProbs;
-
 import java.util.Arrays;
+import java.util.Hashtable;
 
 class IsUnique {
     public static void main(String[] args) {
         char[] str = { 'm', 'a', 'g', 'i', 'e' };
+
         var bruteForceResult = bruteForce(str);
         System.out.println("Brute Force Result " + bruteForceResult);
+
+        var hashTableResult = hashTableSoln(str);
+        System.out.println("Hash Table Result " + hashTableResult);
+
         var OptimizeSolnResult = OptimizeSoln(str);
         System.out.println("Optimal Result " + OptimizeSolnResult);
     }
@@ -23,6 +27,20 @@ class IsUnique {
         return true;
     }
 
+    // O(N)
+    public static Boolean hashTableSoln(char[] input) {
+        Hashtable<Integer, Character> hashtable = new Hashtable<>();
+        for (int i = 0; i < input.length; i++) {
+            if (hashtable.contains(input[i])) {
+                return false;
+            } else {
+                int hashValue = input[i] % input.length;
+                hashtable.put(hashValue, input[i]);
+            }
+        }
+        return true;
+    }
+
     // O(n log n)
     public static Boolean OptimizeSoln(char[] input) {
         Arrays.sort(input); // uses binary sort (n log n)
@@ -33,9 +51,4 @@ class IsUnique {
         }
         return true;
     }
-
-    /*
-     * Without Extra Data Structure: Using bitChecker -> O(n) Ref:
-     * https://www.geeksforgeeks.org/determine-string-unique-characters/
-     */
 }
