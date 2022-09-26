7bac5d09382d3b15533d05891a75ecaa9e3d71c9
Maggie Yogeshvar <yogeshvar@icloud.com>
2022-09-26 21:11:10 +0930

    ✨ day 4 added

--- Prep/Day 3/notes.md
+++ Prep/Day 3/notes.md
@@ -8,3 +8,5 @@
 - Insert => `O(N)` (Why? Cause we copy the array and insert n + 1 element)
   - Insert End in Dynamic => `O(1)`
   - When Size is full, the next insert is `O(n)`
+- Amortized Analysis
+  - Dynamic Array insertion is `O(1)`, but edge cases it is `O(n)`
---
+++ Prep/Day 4/AlgoExpert/TwoNumSum.py
@@ -0,0 +1,42 @@
+"""
+AlgoExpert Problem: Two Number Sum
+Difficulty: Easy 🟢
+"""
+
+# Solution One: Classic Iteration to get all possible answers [O(n^2)]
+
+
+from typing import Set
+
+
+def twoNumberSum_One(array, targetSum):
+    for idx_i, i in enumerate(array):
+        for idx_j, j in enumerate(array):
+            # given constraint
+            if idx_i == idx_j:
+                continue
+            elif i + j == targetSum:
+                return [i, j]
+    pass
+
+# Solution Two: X + Y = TargetSum => X = TargetSum - Y
+
+
+def twoNumberSum_Two(array, targetSum):
+    for idx_i, i in enumerate(array):
+        for idx_j, j in enumerate(array):
+            if idx_i == idx_j:
+                continue
+            elif i == targetSum - j:
+                return [i, j]
+
+# Solution Three: Keep the X = TargetSum - Y in set
+
+
+def twoNumberSum_Three(array, targetSum):
+    seen = set()
+    for y in array:
+        if (targetSum - y) in seen:
+            return [y, (targetSum - y)]
+        seen.add(targetSum - y)
+    return []
