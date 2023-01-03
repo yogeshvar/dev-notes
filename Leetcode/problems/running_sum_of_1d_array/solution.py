061283f28c50a834ef9ce13f760e048f70f5c118
Yogeshvar <yogeshvar@icloud.com>
2023-01-03 13:01:01 +0000

    Sync LeetCode submission - Running Sum of 1d Array (python3)

--- Leetcode/problems/running_sum_of_1d_array/solution.py
+++ Leetcode/problems/running_sum_of_1d_array/solution.py
@@ -1,12 +1,6 @@
-class Solution(object):
-    def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        for idx, i in enumerate(nums): 
-            if idx == 0: 
-                nums[idx] = i
-            else: 
-                nums[idx] = nums[idx - 1] + nums[idx]
-        return nums
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        res = []
+        for idx, i in enumerate(nums):
+            res.append(i + sum(nums[:idx]))
+        return res
