a9126455c3c79cb68741df72bdf6c73877884624
Yogeshvar <yogeshvar@icloud.com>
2023-01-09 16:53:23 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

---
+++ Leetcode/problems/pascal's_triangle/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        row = [0] * numRows
+        for i in range(numRows):
+            row[i] = [0]*(i + 1)
+            row[i][0] = row[i][i] = 1
+            for j in range(1,i):
+                row[i][j] = row[i-1][j-1] + row[i-1][j]
+        return row
+
