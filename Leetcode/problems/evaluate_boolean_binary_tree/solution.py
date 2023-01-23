0a835937a6e220bf34c0f988a2cabefa6aeb30c0
Yogeshvar <yogeshvar@icloud.com>
2023-01-23 14:09:35 +0000

    Sync LeetCode submission - Evaluate Boolean Binary Tree (python3)

---
+++ Leetcode/problems/evaluate_boolean_binary_tree/solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if not root.left and not root.right:
+            return bool(root.val)
+
+        if root.val == 2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        else:
+            return self.evaluateTree(root.left) and self.evaluateTree(root.right)
