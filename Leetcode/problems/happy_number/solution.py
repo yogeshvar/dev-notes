a37e6d8bbbe4a374f9799cf4d43d30a8878dbcfb
Yogeshvar <yogeshvar@icloud.com>
2023-01-27 15:47:22 +0000

    Sync LeetCode submission - Happy Number (python3)

---
+++ Leetcode/problems/happy_number/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum(int(i)**2 for i in str(n))
+        return n == 1
