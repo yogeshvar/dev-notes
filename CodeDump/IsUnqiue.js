c0363e749d4a085bfed86db6d4ced8874343f6ad
Maggie Yogeshvar <yogeshvar@icloud.com>
2023-02-10 19:49:40 +1030

    🔥 remove cracking the code interview qns from codedump

--- CodeDump/IsUnqiue.js
+++
@@ -1,3 +0,0 @@
-var x = [1, 2, 3, 4, 5, 4, 6, 7, 1];
-var set = new Set(x);
-console.log(Array.from(set));
--- CodeDump/StringRotation.py
+++
@@ -1,8 +0,0 @@
-# 1.8 Cracking the coding interview.
-def stringRotation(s1, s2): 
-    if len(s1) == len(s2) and len(s1) > 0:
-        s1s1 = s1 + s1
-        return s2 in s1s1
-    return False
-
-x = stringRotation('waterbottle', 'erbottlewat')
