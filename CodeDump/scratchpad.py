f04695d3b0afc089e3b5837585f3ac99c1d328ab
Yogeshvar <yogeshvar@icloud.com>
2023-02-17 22:32:18 +1030

    ⚗️ playground changes

--- CodeDump/scratchpad.py
+++ CodeDump/scratchpad.py
@@ -1,31 +1,9 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+nums = [1,2,3,4]
 
-i = ListNode()
-i.val = 1 
-i.next = ListNode()
-i.next.val = 1
-i.next.next = ListNode()
-i.next.next.val = 2
+def decompressRLElist(nums):
+    result = []
+    for i in range(0,len(nums),2):
+        result += nums[i]*[nums[i+1]]
+    return result
 
-def print_lst(head):
-    while head != None:
-        print(head.val)
-        head = head.next
-
-# print_lst(i)
-
-def delDup(head):
-    scan = head
-    while head and scan.next:
-        if scan.next.val == scan.val:
-            scan.next = scan.next.next
-        else:
-            scan = scan.next
-    return head
-
-h = delDup(i)    
-print_lst(h)
-    
+x = decompressRLElist(nums)
