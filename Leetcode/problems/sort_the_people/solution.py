e18b2c74f1e2ac8e3e4f2aec3e3447690a59b094
Yogeshvar <yogeshvar@icloud.com>
2023-02-19 12:52:05 +0000

    Sync LeetCode submission - Sort the People (python)

---
+++ Leetcode/problems/sort_the_people/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        return [name for height, name in sorted(zip(heights, names), reverse=True)]
