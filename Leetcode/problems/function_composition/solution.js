6adb969551e5a1f468eae1a83f9907e00d159ce9
Yogeshvar <yogeshvar@icloud.com>
2023-04-18 01:56:41 +0000

    Sync LeetCode submission - Function Composition (javascript)

---
+++ Leetcode/problems/function_composition/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+         return functions.reduceRight(function (arg, fn) {
+            return fn(arg);
+        }, x);
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
